1dacd0e277b58e8376a2b1abe6b5b416bbb2e3c2
Madroxprime <madroxprime@Gmail.com>
2017-10-30 16:48:37 -0500

    Update index.js
    Added JSdoc comments

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const spawn = require('child_process').spawn;
 
+/**
+ *Shuts down specified computer and then performs callback
+ * @param {String} computerName - name of targeted device
+ * @param {callbackFunction} cb - function to perform after shutdown command finishes
+ */
+
 function shutdown(computerName, cb){
     let child = spawn('powershell.exe',['stop-computer -computername '+computerName +' -force']);
 
